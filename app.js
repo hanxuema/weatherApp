1632af4ddb82f4bcdfcc6ab6013470b62dcea06a
Xavier Xie <xieyuehao@gmail.com>
2019-01-16 15:52:58 +1000

    add google maps api, install request module

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-var index = require('./index');
+const request = require("request");
+const fs = require("fs");
+
+
+request(
+  {
+    url:
+      "https://maps.googleapis.com/maps/api/geocode/json?address=brisbane&key=AIzaSyBRdeGz4SkiQDIK4zjZ2at9kQ0ppubaqvM",
+    json: true
+  },
+  (err, response, body) => {
+    fs.writeFile("address.txt", body.results[0].formatted_address, () => {
+      console.log(JSON.stringify(body, undefined, 2));
+    });
+  }
+);
